6494f4de74c8ae0b42fce296f4323112550de730
ElenaZhukouskaya <elenazhukouskaya@gmail.com>
2022-05-02 13:51:29 +0300

    feat: add mine-sweeper solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function minesweeper(matrix) {
+  const resultArr = matrix.map((el) => el.map((e) => 0));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      //существование верхнего ряда:
+      //верхний элемент:
+      if (matrix[i - 1] && matrix[i - 1][j]) {
+        resultArr[i][j] += 1;
+      }
+      //верхний элемент по правой диагонали:
+      if (matrix[i - 1] && matrix[i - 1][j + 1]) {
+        resultArr[i][j] += 1;
+      }
+      //верхний элемент по левой диагонали:
+      if (matrix[i - 1] && matrix[i - 1][j - 1]) {
+        resultArr[i][j] += 1;
+      }
+      // существование нижнего ряда:
+      //нижний элемент:
+      if (matrix[i + 1] && matrix[i + 1][j]) {
+        resultArr[i][j] += 1;
+      }
+      //нижний элемент по правой диагонали:
+      if (matrix[i + 1] && matrix[i + 1][j + 1]) {
+        resultArr[i][j] += 1;
+      }
+      //нижний элемент по левой диагонали:
+      if (matrix[i + 1] && matrix[i + 1][j - 1]) {
+        resultArr[i][j] += 1;
+      }
+      //соседние элементы по ряду:
+      if (matrix[i] && matrix[i][j - 1]) {
+        resultArr[i][j] += 1;
+      }
+      if (matrix[i] && matrix[i][j + 1]) {
+        resultArr[i][j] += 1;
+      }
+    }
+  }
+  return resultArr;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
